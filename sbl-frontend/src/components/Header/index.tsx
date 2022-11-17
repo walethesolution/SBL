57096fb8c96394441f3c6beea129a99964e9d64f
Olawale Ayejuyone <olawaleayejuyone@gmail.com>
2022-11-17 01:03:34 -0500

    Add store logo and user signin button on nav

--- sbl-frontend/src/components/Header/index.tsx
+++ sbl-frontend/src/components/Header/index.tsx
@@ -1,16 +1,22 @@
 import Logo from "../../assets/sbl-logo.png";
+import "./index.css";
 import { AppBar, Button, IconButton, Stack, Toolbar } from "@mui/material";
+import ShoppingCartTwoToneIcon from "@mui/icons-material/ShoppingCartTwoTone";
 
 const Header = () => {
   return (
-    <AppBar position="static">
+    <AppBar position="static" style={{ background: "black" }}>
       <Toolbar>
-        <IconButton>SBL</IconButton>
+        <IconButton color="inherit">SBL</IconButton>
         <Stack direction="row" spacing={2}>
           <Button color="inherit">Games</Button>
           <Button color="inherit">Stats</Button>
           <Button color="inherit">Teams</Button>
           <Button color="inherit">Players</Button>
+          <Button color="inherit">
+            <ShoppingCartTwoToneIcon />
+          </Button>
+          <Button color="inherit">User SignIn</Button>
         </Stack>
       </Toolbar>
     </AppBar>
