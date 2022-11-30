e783ba51614167a895be5c99f8c82d693ab22717
Olawale Ayejuyone <olawaleayejuyone@gmail.com>
2022-11-29 20:20:25 -0500

    Add Image, text and button to home page

--- sbl-frontend/src/components/Body/index.tsx
+++ sbl-frontend/src/components/Body/index.tsx
@@ -1,27 +1,41 @@
 import Grid from "@mui/material/Grid";
 import { Typography, Box, Button } from "@mui/material";
-import sbl from "../../assets/sbl-logo.png";
+// import sbl from "../../assets/sbl-logo.png";
+import mainPic from "../../assets/fall-ball3.jpg";
 
 export default function HomePage() {
+  const imageStyles = {
+    width: "100%",
+    // height: "590px",
+    alignItems: "center",
+    marginBottom: "30px",
+  };
+
+  const buttonStyles = {
+    width: "80%",
+  };
   return (
-    <Box>
-      <Grid
-        container
-        spacing={4}
-        sx={{ justifyContent: "center", alignItems: "center" }}
-      >
+    <Box
+      sx={{ marginTop: "40px", backgroundColor: "rgba(128, 128, 128, 0.08)" }}
+    >
+      <Grid container spacing={4} sx={{ alignItems: "center" }}>
         <Grid item xs={12} md={6}>
-          <img src={sbl} alt="My Team" />
+          <img src={mainPic} alt="My Team" style={imageStyles} />
         </Grid>
         <Grid item xs={12} md={6}>
-          <Typography variant="h2" fontWeight="700">
+          <Typography variant="h1" fontWeight="700">
             Everybody's League
           </Typography>
-          <Typography variant="h6">
-            Pros in the community. Spoiler alert: we are all pros in this
-            community.
+          <Typography variant="h6" sx={{ align: "center" }}>
+            Community professionals
           </Typography>
-          <Button variant="contained" color="primary">
+          <Button
+            variant="contained"
+            color="success"
+            sx={{ marginTop: "40px" }}
+            style={buttonStyles}
+            size="large"
+          >
             Come join us
           </Button>
         </Grid>
