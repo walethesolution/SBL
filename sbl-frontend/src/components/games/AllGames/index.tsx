8501992a9d355c7033ccdb2ca54a3f9877391702
Olawale Ayejuyone <olawaleayejuyone@gmail.com>
2022-12-19 17:38:51 -0500

    Change the button component on games page to Tabs component

--- sbl-frontend/src/components/games/AllGames/index.tsx
+++ sbl-frontend/src/components/games/AllGames/index.tsx
@@ -6,12 +6,16 @@ import {
   Typography,
   Card,
   CardContent,
+  Tabs,
+  Tab,
 } from "@mui/material";
+import { Stack } from "@mui/system";
 import { DummyImage } from "react-simple-placeholder-image";
 
 const dummyStyle = {
   borderRadius: 50,
 };
+
 const buttonStyle = {
   backgroundColor: "black",
   color: "white",
@@ -20,45 +24,58 @@ const buttonStyle = {
 export default function AllGames() {
   return (
     <Box>
-      <Button style={buttonStyle} variant="contained">
-        All games
-      </Button>
-      <Button color="inherit">Highlight of the week</Button>
-      <Box display="flex" justifyContent="center">
-        <Card sx={{ backgroundColor: "rgba(0, 0, 0, 0.35)" }}>
-          <CardContent>Month, Year</CardContent>
-        </Card>
-        <Box>
-          <Grid>
-            <Grid item xs={12} md={6}>
-              <Card sx={{ backgroundColor: "rgba(0, 0, 0, 0.35)" }}>
-                <DummyImage
-                  width={100}
-                  height={100}
-                  shape="image"
-                  style={dummyStyle}
-                />
-                <CardContent>
-                  <Typography>TEAM A</Typography>
-                </CardContent>
-                <DummyImage
-                  width={100}
-                  height={100}
-                  shape="image"
-                  style={dummyStyle}
-                />
-                <CardContent>
-                  <Typography>TEAM B</Typography>
-                </CardContent>
-                <Divider variant="middle" color="grey" />
-                <Box>
-                  <Button color="inherit">Highlight</Button>
-                  <Button color="inherit">Box Score</Button>
-                </Box>
-              </Card>
+      <Tabs>
+        <Tab label="All games" />
+        <Tab label="Highlight of the week" />
+      </Tabs>
+
+      <Box
+        sx={{
+          marginLeft: "15%",
+          marginTop: "5%",
+          width: "70%",
+        }}
+      >
+        <Stack>
+          <Card>
+            <CardContent>Month, Year</CardContent>
+          </Card>
+          <Box>
+            <Grid>
+              <Grid item xs={12} md={6}>
+                <Card
+                  sx={{
+                    marginTop: "2%",
+                  }}
+                >
+                  <DummyImage
+                    width={100}
+                    height={100}
+                    shape="image"
+                    style={dummyStyle}
+                  />
+                  <CardContent>
+                    <Typography>TEAM A</Typography>
+                  </CardContent>
+                  <DummyImage
+                    width={100}
+                    height={100}
+                    shape="image"
+                    style={dummyStyle}
+                  />
+                  <CardContent>
+                    <Typography>TEAM B</Typography>
+                  </CardContent>
+                  <Divider variant="middle" color="grey" />
+                  <Box>
+                    <Button color="inherit">Highlight</Button>
+                    <Button color="inherit">Box Score</Button>
+                  </Box>
+                </Card>
+              </Grid>
             </Grid>
-          </Grid>
-        </Box>
+          </Box>
+        </Stack>
       </Box>
     </Box>
   );
