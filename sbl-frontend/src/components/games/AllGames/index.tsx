045c40cc3873633ee425f618591ce4e98e1403d2
Olawale Ayejuyone <olawaleayejuyone@gmail.com>
2022-12-19 14:29:11 -0500

    Change theme of game page
    Change Button style

--- sbl-frontend/src/components/games/AllGames/index.tsx
+++ sbl-frontend/src/components/games/AllGames/index.tsx
@@ -3,12 +3,9 @@ import {
   Grid,
   Button,
   Divider,
-  Stack,
   Typography,
   Card,
   CardContent,
-  CardMedia,
-  Toolbar,
 } from "@mui/material";
 import { Form } from "react-router-dom";
 import { DummyImage } from "react-simple-placeholder-image";
@@ -16,64 +13,66 @@ import { DummyImage } from "react-simple-placeholder-image";
 const dummyStyle = {
   borderRadius: 50,
 };
+const buttonStyle = {
+  backgroundColor: "black",
+  color: "white",
+};
 
 export default function AllGames() {
   return (
-    <Box sx={{ backgroundColor: "rgba(55, 146, 211, 0.51)" }}>
-      <Toolbar sx={{ marginBottom: "20px" }}>
-        <Grid>
-          <Grid item>
-            <Button>All games</Button>
-          </Grid>
-          <Grid item>
-            <Button>Highlight of the week</Button>
-          </Grid>
+    <Box>
+      <Grid>
+        <Grid item>
+          <Button style={buttonStyle} variant="text">
+            All games
+          </Button>
         </Grid>
-      </Toolbar>
+        <Grid item>
+          <Button color="inherit">Highlight of the week</Button>
+        </Grid>
+      </Grid>
       <Box>
-        <Stack>
-          <Card sx={{ backgroundColor: "rgba(0, 0, 0, 0.35)" }}>
-            <CardContent>Month, Year</CardContent>
-          </Card>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-            }}
-          >
-            <Grid>
-              <Grid item xs={12} md={6}>
-                <Card sx={{ backgroundColor: "rgba(0, 0, 0, 0.35)" }}>
-                  <DummyImage
-                    width={100}
-                    height={100}
-                    shape="image"
-                    style={dummyStyle}
-                  />
-                  <CardContent>
-                    <Typography>TEAM A</Typography>
-                  </CardContent>
-                  <DummyImage
-                    width={100}
-                    height={100}
-                    shape="image"
-                    style={dummyStyle}
-                  />
-                  <CardContent>
-                    <Typography>TEAM B</Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Divider variant="middle" color="grey" />
-              <Form>
-                <Button>Highlight</Button>
-              </Form>
-              <Form>
-                <Button>Box Score</Button>
-              </Form>
+        <Card sx={{ backgroundColor: "rgba(0, 0, 0, 0.35)" }}>
+          <CardContent>Month, Year</CardContent>
+        </Card>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Grid>
+            <Grid item xs={12} md={6}>
+              <Card sx={{ backgroundColor: "rgba(0, 0, 0, 0.35)" }}>
+                <DummyImage
+                  width={100}
+                  height={100}
+                  shape="image"
+                  style={dummyStyle}
+                />
+                <CardContent>
+                  <Typography>TEAM A</Typography>
+                </CardContent>
+                <DummyImage
+                  width={100}
+                  height={100}
+                  shape="image"
+                  style={dummyStyle}
+                />
+                <CardContent>
+                  <Typography>TEAM B</Typography>
+                </CardContent>
+              </Card>
             </Grid>
-          </Box>
-        </Stack>
+            <Divider variant="middle" color="grey" />
+            <Form>
+              <Button>Highlight</Button>
+            </Form>
+            <Form>
+              <Button>Box Score</Button>
+            </Form>
+          </Grid>
+        </Box>
       </Box>
     </Box>
   );
