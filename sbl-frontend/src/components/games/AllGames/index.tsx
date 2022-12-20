e73aeb690d25beedef027edd073b78228a3b858f
Olawale Ayejuyone <olawaleayejuyone@gmail.com>
2022-12-19 21:43:11 -0500

    Change Games layout

--- sbl-frontend/src/components/games/AllGames/index.tsx
+++ sbl-frontend/src/components/games/AllGames/index.tsx
@@ -1,5 +1,4 @@
 import {
-  AppBar,
   Box,
   Grid,
   Button,
@@ -19,63 +18,68 @@ const dummyStyle = {
 
 export default function AllGames() {
   return (
-    <Box sx={{ backgroundColor: "rgba(55, 146, 211, 0.08)" }}>
-      <AppBar
-        position="static"
-        style={{ backgroundColor: "rgba(128, 128, 128, 0.30)" }}
-      >
-        <Tabs>
+    <Box>
+      <Box sx={{ backgroundColor: "rgba(55, 146, 211, 0.08)" }}>
+        <Tabs style={{ backgroundColor: "rgba(128, 128, 128, 0.30)" }}>
           <Tab value="0" label="All games" />
           <Tab value="1" label="Highlight of the week" />
         </Tabs>
-      </AppBar>
-      <Box
-        sx={{
-          marginLeft: "15%",
-          marginTop: "2%",
-          width: "70%",
-        }}
-      >
-        <Stack>
-          <Card>
-            <CardContent>Month, Year</CardContent>
-          </Card>
-          <Box>
-            <Grid>
-              <Grid item xs={12} md={6}>
-                <Card
-                  sx={{
-                    marginTop: "2%",
-                  }}
-                >
-                  <DummyImage
-                    width={100}
-                    height={100}
-                    shape="image"
-                    style={dummyStyle}
-                  />
-                  <CardContent>
-                    <Typography>TEAM A</Typography>
-                  </CardContent>
-                  <DummyImage
-                    width={100}
-                    height={100}
-                    shape="image"
-                    style={dummyStyle}
-                  />
-                  <CardContent>
-                    <Typography>TEAM B</Typography>
-                  </CardContent>
-                  <Divider variant="middle" color="grey" />
-                  <Box>
-                    <Button color="inherit">Highlight</Button>
-                    <Button color="inherit">Box Score</Button>
-                  </Box>
-                </Card>
+        <Box
+          sx={{
+            marginLeft: "15%",
+            marginTop: "2%",
+            width: "70%",
+          }}
+        >
+          <Stack>
+            <Card>
+              <CardContent>Month, Year</CardContent>
+            </Card>
+            <Box>
+              <Grid>
+                <Grid item>
+                  <Card
+                    sx={{
+                      marginTop: "2%",
+                    }}
+                  >
+                    <Grid container sx={{ justifyContent: "space-evenly" }}>
+                      <Grid item>
+                        <CardContent> </CardContent>
+                        <DummyImage
+                          width={100}
+                          height={100}
+                          shape="image"
+                          style={dummyStyle}
+                        />
+                        <CardContent>
+                          <Typography>TEAM A</Typography>
+                        </CardContent>
+                      </Grid>
+                      <Grid item>
+                        <CardContent> </CardContent>
+                        <DummyImage
+                          width={100}
+                          height={100}
+                          shape="image"
+                          style={dummyStyle}
+                        />
+                        <CardContent>
+                          <Typography>TEAM B</Typography>
+                        </CardContent>
+                      </Grid>
+                    </Grid>
+                    <Divider variant="middle" color="grey" />
+                    <Box sx={{ justifyContent: "center" }}>
+                      <Button color="inherit">Highlight</Button>
+                      <Button color="inherit">Box Score</Button>
+                    </Box>
+                  </Card>
+                </Grid>
               </Grid>
-            </Grid>
-          </Box>
-        </Stack>
+            </Box>
+          </Stack>
+        </Box>
       </Box>
     </Box>
   );
