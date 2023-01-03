59b072198bfab7548e42179b2356f35baa8234da
Olawale Ayejuyone <olawaleayejuyone@gmail.com>
2023-01-03 14:22:59 -0500

    Add Header and Footer component to every page for quick UI test
    Add link to logo

--- sbl-frontend/src/components/games/index.tsx
+++ sbl-frontend/src/components/games/index.tsx
@@ -12,6 +12,8 @@ import {
 } from "@mui/material";
 import { Form } from "react-router-dom";
 import { DummyImage } from "react-simple-placeholder-image";
+import Footer from "../landing-page/Footer";
+import Header from "../landing-page/Header";
 
 const dummyStyle = {
   borderRadius: 50,
@@ -20,167 +22,175 @@ const dummyStyle = {
 export default function Games() {
   return (
     <Box>
-      <Box sx={{ backgroundColor: "rgba(55, 146, 211, 0.08)" }}>
-        <Tabs style={{ backgroundColor: "rgba(128, 128, 128, 0.30)" }}>
-          <Tab value="0" label="All games" />
-          <Tab value="1" label="Highlight of the week" />
-        </Tabs>
-        <Box
-          sx={{
-            marginLeft: "15%",
-            marginTop: "2%",
-            width: "70%",
-          }}
-        >
-          <Stack>
-            <Card>
-              <CardContent>Month, Year</CardContent>
-            </Card>
-            <Box>
-              <Grid>
-                <Grid item>
-                  <Card
-                    sx={{
-                      marginTop: "2%",
-                    }}
-                  >
-                    <Grid
-                      container
+      <Header />
+      <Box>
+        <Box sx={{ backgroundColor: "rgba(55, 146, 211, 0.08)" }}>
+          <Tabs style={{ backgroundColor: "rgba(128, 128, 128, 0.30)" }}>
+            <Tab value="0" label="All games" />
+            <Tab value="1" label="Highlight of the week" />
+          </Tabs>
+          <Box
+            sx={{
+              marginLeft: "15%",
+              marginTop: "2%",
+              width: "70%",
+            }}
+          >
+            <Stack>
+              <Card>
+                <CardContent>Month, Year</CardContent>
+              </Card>
+              <Box>
+                <Grid>
+                  <Grid item>
+                    <Card
                       sx={{
-                        justifyContent: "space-evenly",
-                        alignItems: "center",
+                        marginTop: "2%",
                       }}
                     >
-                      <Grid item>
-                        <Grid
-                          container
-                          spacing={2}
-                          sx={{ alignItems: "center" }}
-                        >
-                          <Grid item>
-                            <CardContent> </CardContent>
-                            <DummyImage
-                              width={100}
-                              height={100}
-                              shape="image"
-                              style={dummyStyle}
-                            />
-                            <CardContent>
-                              <Typography>TEAM A</Typography>
-                            </CardContent>
+                      <Grid
+                        container
+                        sx={{
+                          justifyContent: "space-evenly",
+                          alignItems: "center",
+                        }}
+                      >
+                        <Grid item>
+                          <Grid
+                            container
+                            spacing={2}
+                            sx={{ alignItems: "center" }}
+                          >
+                            <Grid item>
+                              <CardContent> </CardContent>
+                              <DummyImage
+                                width={100}
+                                height={100}
+                                shape="image"
+                                style={dummyStyle}
+                              />
+                              <CardContent>
+                                <Typography>TEAM A</Typography>
+                              </CardContent>
+                            </Grid>
+                            <Grid item>
+                              <CardContent>
+                                <Typography variant="h2">7</Typography>
+                              </CardContent>
+                            </Grid>
                           </Grid>
-                          <Grid item>
-                            <CardContent>
-                              <Typography variant="h2">7</Typography>
-                            </CardContent>
+                        </Grid>
+                        <Grid item>
+                          <Typography>FINAL</Typography>
+                        </Grid>
+                        <Grid item>
+                          <Grid
+                            container
+                            spacing={2}
+                            sx={{ alignItems: "center" }}
+                          >
+                            <Grid item>
+                              <CardContent>
+                                <Typography variant="h2">5</Typography>
+                              </CardContent>
+                            </Grid>
+                            <Grid item>
+                              <CardContent> </CardContent>
+                              <DummyImage
+                                width={100}
+                                height={100}
+                                shape="image"
+                                style={dummyStyle}
+                              />
+                              <CardContent>
+                                <Typography>TEAM B</Typography>
+                              </CardContent>
+                            </Grid>
                           </Grid>
                         </Grid>
                       </Grid>
-                      <Grid item>
-                        <Typography>FINAL</Typography>
-                      </Grid>
-                      <Grid item>
-                        <Grid
-                          container
-                          spacing={2}
-                          sx={{ alignItems: "center" }}
-                        >
-                          <Grid item>
-                            <CardContent>
-                              <Typography variant="h2">5</Typography>
-                            </CardContent>
-                          </Grid>
-                          <Grid item>
-                            <CardContent> </CardContent>
-                            <DummyImage
-                              width={100}
-                              height={100}
-                              shape="image"
-                              style={dummyStyle}
-                            />
-                            <CardContent>
-                              <Typography>TEAM B</Typography>
-                            </CardContent>
-                          </Grid>
-                        </Grid>
-                      </Grid>
-                    </Grid>
-                    <Divider variant="middle" />
-                    <Box sx={{ display: "flex", justifyContent: "center" }}>
-                      <Form>
-                        <Button color="inherit">Highlight</Button>
-                      </Form>
-                      <Divider orientation="vertical" flexItem />
-                      <Form>
-                        <Button color="inherit">Box Score</Button>
-                      </Form>
-                    </Box>
-                  </Card>
+                      <Divider variant="middle" />
+                      <Box
+                        sx={{ display: "flex", justifyContent: "space-evenly" }}
+                      >
+                        <Form>
+                          <Button color="inherit">Highlight</Button>
+                        </Form>
+                        <Divider orientation="vertical" flexItem />
+                        <Form>
+                          <Button color="inherit">Box Score</Button>
+                        </Form>
+                      </Box>
+                    </Card>
+                  </Grid>
                 </Grid>
-              </Grid>
-            </Box>
-            <Box>
-              <Grid>
-                <Grid item>
-                  <Card
-                    sx={{
-                      marginTop: "2%",
-                    }}
-                  >
-                    <Grid
-                      container
+              </Box>
+              <Box>
+                <Grid>
+                  <Grid item>
+                    <Card
                       sx={{
-                        justifyContent: "space-evenly",
-                        alignItems: "center",
+                        marginTop: "2%",
                       }}
                     >
-                      <Grid item>
-                        <CardContent> </CardContent>
-                        <DummyImage
-                          width={100}
-                          height={100}
-                          shape="image"
-                          style={dummyStyle}
-                        />
-                        <CardContent>
-                          <Typography>TEAM C</Typography>
-                        </CardContent>
+                      <Grid
+                        container
+                        sx={{
+                          justifyContent: "space-evenly",
+                          alignItems: "center",
+                        }}
+                      >
+                        <Grid item>
+                          <CardContent> </CardContent>
+                          <DummyImage
+                            width={100}
+                            height={100}
+                            shape="image"
+                            style={dummyStyle}
+                          />
+                          <CardContent>
+                            <Typography>TEAM C</Typography>
+                          </CardContent>
+                        </Grid>
+                        <Grid item>
+                          <Stack>
+                            <Typography variant="h4">10:00 AM ET</Typography>
+                          </Stack>
+                        </Grid>
+                        <Grid item>
+                          <CardContent> </CardContent>
+                          <DummyImage
+                            width={100}
+                            height={100}
+                            shape="image"
+                            style={dummyStyle}
+                          />
+                          <CardContent>
+                            <Typography>TEAM D</Typography>
+                          </CardContent>
+                        </Grid>
                       </Grid>
-                      <Grid item>
-                        <Stack>
-                          <Typography variant="h4">10:00 AM ET</Typography>
-                        </Stack>
-                      </Grid>
-                      <Grid item>
-                        <CardContent> </CardContent>
-                        <DummyImage
-                          width={100}
-                          height={100}
-                          shape="image"
-                          style={dummyStyle}
-                        />
-                        <CardContent>
-                          <Typography>TEAM D</Typography>
-                        </CardContent>
-                      </Grid>
-                    </Grid>
-                    <Divider variant="middle" />
-                    <Box sx={{ display: "flex", justifyContent: "center" }}>
-                      <Form>
-                        <Button color="inherit">Line-up</Button>
-                      </Form>
-                      <Divider orientation="vertical" flexItem />
-                      <Form>
-                        <Button color="inherit">Box Score</Button>
-                      </Form>
-                    </Box>
-                  </Card>
+                      <Divider variant="middle" />
+                      <Box
+                        sx={{ display: "flex", justifyContent: "space-evenly" }}
+                      >
+                        <Form>
+                          <Button color="inherit">Line-up</Button>
+                        </Form>
+                        <Divider orientation="vertical" flexItem />
+                        <Form>
+                          <Button color="inherit">Box Score</Button>
+                        </Form>
+                      </Box>
+                    </Card>
+                  </Grid>
                 </Grid>
-              </Grid>
-            </Box>
-          </Stack>
+              </Box>
+            </Stack>
+          </Box>
         </Box>
       </Box>
+      <Footer />
     </Box>
   );
 }
