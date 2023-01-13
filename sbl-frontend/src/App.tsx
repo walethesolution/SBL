8fd1e774e18f9853fb4c5974b6663be7906c51b4
Olawale Ayejuyone <olawaleayejuyone@gmail.com>
2023-01-13 12:19:00 -0500

    Testing different conditional logic to display paths to different pages properly

--- sbl-frontend/src/App.tsx
+++ sbl-frontend/src/App.tsx
@@ -1,36 +1,56 @@
-import React from "react";
-import Layout from "./components/Layout";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Router,
-  Route,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useLocation } from "react-router";
 import { Box } from "@mui/material";
-import Games from "./components/games";
-import Stats from "./components/stats";
-import Players from "./components/players";
-import About from "./components/about";
+import Layout from "./components/Layout";
+// import Games from "./components/games";
+// import Stats from "./components/stats";
+// import Players from "./components/players";
+// import About from "./components/about";
 import ErrorPage from "./components/errors";
+// import FirstPage from "./components/firstpage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,
+    element: <Layout page="home" />,
     errorElement: <ErrorPage />,
     children: [
-      { path: "/games", element: <Games /> },
-      { path: "/about", element: <About /> },
-      { path: "/stats", element: <Stats /> },
-      { path: "/players", element: <Players /> },
+      { path: "/games", element: <Layout page="games" /> },
+      { path: "/about", element: <Layout page="about" /> },
+      { path: "/stats", element: <Layout page="stats" /> },
+      { path: "/players", element: <Layout page="players" /> },
     ],
   },
 ]);
 
 export default function App() {
+  const location = useLocation();
+  let component;
+
+  switch (location.pathname) {
+    case "/":
+      component = <Layout page="home" />;
+      break;
+    case "/games":
+      component = <Layout page="games" />;
+      break;
+    case "/about":
+      component = <Layout page="about" />;
+      break;
+    case "/stats":
+      component = <Layout page="stats" />;
+      break;
+    case "/players":
+      component = <Layout page="players" />;
+      break;
+    default:
+      component = <ErrorPage />;
+  }
+
   return (
     <Box>
       <RouterProvider router={router} />
+      {component}
     </Box>
   );
 }
--- sbl-frontend/src/components/Layout/index.tsx
+++ sbl-frontend/src/components/Layout/index.tsx
@@ -3,13 +3,40 @@ import Header from "../landing-page/Header";
 import Footer from "../landing-page/Footer";
 import { Box } from "@mui/material";
 import FirstPage from "../firstpage";
+import Games from "../games";
+import About from "../about";
+import Stats from "../stats";
+import Players from "../players";
+import ErrorPage from "../errors";
 import { Outlet } from "react-router-dom";
 
-export default function Layout() {
+export default function Layout({ page }: { page: string }) {
+  let component;
+
+  // switch (page) {
+  //   case "home":
+  //     component = <FirstPage />;
+  //     break;
+  //   case "about":
+  //     component = <About />;
+  //     break;
+  //   case "games":
+  //     component = <Games />;
+  //     break;
+  //   case "stats":
+  //     component = <Stats />;
+  //     break;
+  //   case "players":
+  //     component = <Players />;
+  //     break;
+  //   default:
+  //     component = <ErrorPage />;
+  // }
+
   return (
     <Box sx={{ backgroundColor: "rgba(55, 146, 211, 0.08)" }}>
       <Header />
-      <FirstPage />
+      {component}
       <Box>
         <Outlet />
       </Box>
