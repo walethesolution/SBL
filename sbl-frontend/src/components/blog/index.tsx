025ec68dd76f9ecb07959b67b5418ca2ee32b10e
Olawale Ayejuyone <olawaleayejuyone@gmail.com>
2023-01-14 17:26:30 -0500

    Add blog tab

--- sbl-frontend/src/components/blog/index.tsx
+++ sbl-frontend/src/components/blog/index.tsx
@@ -1,16 +1,21 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography, Tabs, Tab } from "@mui/material";
 
 export default function Blog() {
   return (
-    <Box sx={{ margin: 25 }}>
-      <Grid container spacing={4}>
-        <Grid item>
-          <Typography variant="h2">Blog Page is coming soon</Typography>
+    <Box>
+      <Tabs style={{ backgroundColor: "rgba(128, 128, 128, 0.30)" }}>
+        <Tab value="0" label="Blog" />
+      </Tabs>
+      <Box sx={{ margin: 25 }}>
+        <Grid container spacing={4}>
+          <Grid item>
+            <Typography variant="h2">Blog Page is coming soon</Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="h2">Work in Progress</Typography>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Typography variant="h2">Work in Progress</Typography>
-        </Grid>
-      </Grid>
+      </Box>
     </Box>
   );
 }
