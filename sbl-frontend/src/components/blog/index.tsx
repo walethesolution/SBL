e7058396f00dcd19ae4104b1e2588a4f46eca7ed
Olawale Ayejuyone <olawaleayejuyone@gmail.com>
2023-01-14 15:36:04 -0500

    Add Blog component

--- sbl-frontend/src/components/blog/index.tsx
+++ sbl-frontend/src/components/blog/index.tsx
@@ -1,1 +1,16 @@
-export default function Blog() {}
+import { Box, Grid, Typography } from "@mui/material";
+
+export default function Blog() {
+  return (
+    <Box sx={{ margin: 25 }}>
+      <Grid container spacing={4}>
+        <Grid item>
+          <Typography variant="h2">Blog Page is coming soon</Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="h2">Work in Progress</Typography>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
