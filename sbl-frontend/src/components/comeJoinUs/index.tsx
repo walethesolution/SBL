ca2e8a1fdf6ca49636b48c9f4ab38aa86565d3dc
Olawale Ayejuyone <olawaleayejuyone@gmail.com>
2023-02-17 12:39:16 -0500

    Add join us page

--- sbl-frontend/src/components/comeJoinUs/index.tsx
+++ sbl-frontend/src/components/comeJoinUs/index.tsx
@@ -1,35 +1,72 @@
 import * as React from "react";
-import { Box, Grid, Typography, TextField } from "@mui/material";
+import { Box, Grid, Typography, TextField, Button } from "@mui/material";
 
 const ComeJoinUs: React.FC = () => {
   return (
-    <Box>
-      <Grid container>
+    <Box margin={10}>
+      <Grid
+        container
+        spacing={4}
+        sx={{ alignItems: "center", justifyContent: "center" }}
+      >
         <Grid item>
-          <Typography variant="h3">Be a part of our community</Typography>
+          <Typography variant="h3">Be a part</Typography>
+          <Typography variant="h3">of our community</Typography>
         </Grid>
-      </Grid>
-      <Grid item>
-        <Box>
-          <Grid container spacing={2}>
+        <Grid item>
+          <Grid container spacing={4} direction="column">
+            <Grid item>
+              <Grid container spacing={2}>
+                <Grid item>
+                  <TextField
+                    id="filled-basic"
+                    label="First Name"
+                    variant="filled"
+                    required
+                    inputProps={{ style: { width: "200px" } }}
+                  />
+                </Grid>
+                <Grid item>
+                  <TextField
+                    id="filled-basic"
+                    label="Last Name"
+                    variant="filled"
+                    required
+                    inputProps={{ style: { width: "200px" } }}
+                  />
+                </Grid>
+              </Grid>
+            </Grid>
+
             <Grid item>
               <TextField
                 id="filled-basic"
-                label="First Name"
+                label="Email"
                 variant="filled"
                 required
+                inputProps={{ style: { width: "440px" } }}
               />
             </Grid>
             <Grid item>
               <TextField
                 id="filled-basic"
-                label="Last Name"
+                label="Country of Origin"
                 variant="filled"
                 required
+                inputProps={{ style: { width: "440px" } }}
               />
+            </Grid>
+            <Grid item>
+              <Button
+                variant="contained"
+                style={{ width: "465px" }}
+                size="large"
+              >
+                Join
+              </Button>
             </Grid>
           </Grid>
-        </Box>
+        </Grid>
       </Grid>
     </Box>
   );
