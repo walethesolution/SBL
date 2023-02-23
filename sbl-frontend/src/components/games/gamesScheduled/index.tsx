a30a9d2b3a84bd1e21ca6a06f3a9673dede8ff73
Olawale Ayejuyone <olawaleayejuyone@gmail.com>
2023-02-23 01:11:29 -0500

    Refactor games page

---
+++ sbl-frontend/src/components/games/gamesScheduled/index.tsx
@@ -0,0 +1,63 @@
+import {
+  Box,
+  Grid,
+  Card,
+  CardContent,
+  Avatar,
+  Typography,
+  Stack,
+  Divider,
+  Button,
+} from "@mui/material";
+
+const GamesSchedule: React.FC = () => {
+  return (
+    <Box>
+      <Grid>
+        <Grid item>
+          <Card
+            sx={{
+              marginTop: "2%",
+            }}
+          >
+            <Grid
+              container
+              sx={{
+                justifyContent: "space-evenly",
+                alignItems: "center",
+              }}
+            >
+              <Grid item>
+                <CardContent> </CardContent>
+                <Avatar sx={{ width: 100, height: 100 }}>C</Avatar>
+                <CardContent>
+                  <Typography>TEAM C</Typography>
+                </CardContent>
+              </Grid>
+              <Grid item>
+                <Stack>
+                  <Typography variant="h4">10:00 AM ET</Typography>
+                </Stack>
+              </Grid>
+              <Grid item>
+                <CardContent> </CardContent>
+                <Avatar sx={{ width: 100, height: 100 }}>D</Avatar>
+                <CardContent>
+                  <Typography>TEAM D</Typography>
+                </CardContent>
+              </Grid>
+            </Grid>
+            <Divider variant="middle" />
+            <Box sx={{ display: "flex", justifyContent: "space-evenly" }}>
+              <Button color="inherit">Line-up</Button>
+              <Divider orientation="vertical" flexItem />
+              <Button color="inherit">Box Score</Button>
+            </Box>
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default GamesSchedule;
--- sbl-frontend/src/components/games/index.tsx
+++ sbl-frontend/src/components/games/index.tsx
@@ -1,17 +1,5 @@
 import * as React from "react";
-import {
-  Box,
-  Grid,
-  Button,
-  Divider,
-  Typography,
-  Card,
-  CardContent,
-  Tabs,
-  Tab,
-  Stack,
-  Avatar,
-} from "@mui/material";
+import { Box, Card, CardContent, Tabs, Tab, Stack } from "@mui/material";
 import TabPanel from "../tabPanel";
 import { a11yProps } from "../../utils";
 import TextField from "@mui/material/TextField";
@@ -19,6 +7,8 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
 import GameDays from "./gamesData";
+import GamesSchedule from "./gamesScheduled";
+import NoGamesScheduled from "./noGamesSchedule";
 
 /**
  * All game days are on sundays, so we need to only show games on Sunday.
@@ -75,130 +65,10 @@ const Games: React.FC = () => {
               </CardContent>
             </Card>
             <TabPanel value={value} index={0}>
-              <Box>
-                <Grid>
-                  <Grid item>
-                    <Card
-                      sx={{
-                        marginTop: "2%",
-                      }}
-                    >
-                      <Grid
-                        container
-                        sx={{
-                          justifyContent: "space-evenly",
-                          alignItems: "center",
-                        }}
-                      >
-                        <Grid item>
-                          <CardContent> </CardContent>
-                          <Avatar sx={{ width: 100, height: 100 }}>C</Avatar>
-                          <CardContent>
-                            <Typography>TEAM C</Typography>
-                          </CardContent>
-                        </Grid>
-                        <Grid item>
-                          <Stack>
-                            <Typography variant="h4">10:00 AM ET</Typography>
-                          </Stack>
-                        </Grid>
-                        <Grid item>
-                          <CardContent> </CardContent>
-                          <Avatar sx={{ width: 100, height: 100 }}>D</Avatar>
-                          <CardContent>
-                            <Typography>TEAM D</Typography>
-                          </CardContent>
-                        </Grid>
-                      </Grid>
-                      <Divider variant="middle" />
-                      <Box
-                        sx={{ display: "flex", justifyContent: "space-evenly" }}
-                      >
-                        <Button color="inherit">Line-up</Button>
-                        <Divider orientation="vertical" flexItem />
-                        <Button color="inherit">Box Score</Button>
-                      </Box>
-                    </Card>
-                  </Grid>
-                </Grid>
-              </Box>
+              <GamesSchedule />
             </TabPanel>
             <TabPanel value={value} index={1}>
-              <Box>
-                <Grid>
-                  <Grid item>
-                    <Card
-                      sx={{
-                        marginTop: "2%",
-                      }}
-                    >
-                      <Grid
-                        container
-                        sx={{
-                          justifyContent: "space-evenly",
-                          alignItems: "center",
-                        }}
-                      >
-                        <Grid item>
-                          <Grid
-                            container
-                            spacing={2}
-                            sx={{ alignItems: "center" }}
-                          >
-                            <Grid item>
-                              <CardContent> </CardContent>
-                              <Avatar sx={{ width: 100, height: 100 }}>
-                                A
-                              </Avatar>
-                              <CardContent>
-                                <Typography>TEAM A</Typography>
-                              </CardContent>
-                            </Grid>
-                            <Grid item>
-                              <CardContent>
-                                <Typography variant="h2">7</Typography>
-                              </CardContent>
-                            </Grid>
-                          </Grid>
-                        </Grid>
-                        <Grid item>
-                          <Typography>FINAL</Typography>
-                        </Grid>
-                        <Grid item>
-                          <Grid
-                            container
-                            spacing={2}
-                            sx={{ alignItems: "center" }}
-                          >
-                            <Grid item>
-                              <CardContent>
-                                <Typography variant="h2">5</Typography>
-                              </CardContent>
-                            </Grid>
-                            <Grid item>
-                              <CardContent> </CardContent>
-                              <Avatar sx={{ width: 100, height: 100 }}>
-                                B
-                              </Avatar>
-                              <CardContent>
-                                <Typography>TEAM B</Typography>
-                              </CardContent>
-                            </Grid>
-                          </Grid>
-                        </Grid>
-                      </Grid>
-                      <Divider variant="middle" />
-                      <Box
-                        sx={{ display: "flex", justifyContent: "space-evenly" }}
-                      >
-                        <Button color="inherit">Highlight</Button>
-                        <Divider orientation="vertical" flexItem />
-                        <Button color="inherit">Box Score</Button>
-                      </Box>
-                    </Card>
-                  </Grid>
-                </Grid>
-              </Box>
+              <NoGamesScheduled />
             </TabPanel>
           </Stack>
         </Box>
